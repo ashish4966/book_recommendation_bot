6622e6be0e042067209583f2115bf6a39ce24322
ashish4966 <111302500+ashish4966@users.noreply.github.com>
2024-05-01 12:13:39 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,136 @@
+import streamlit as st
+import numpy as np
+import pickle
+import random
+import nltk
+import requests
+import json
+nltk.download('punkt')
+nltk.download('wordnet')
+from nltk.stem import WordNetLemmatizer
+lemmatizer = WordNetLemmatizer()
+from bs4 import BeautifulSoup
+from tensorflow.keras.models import load_model
+
+# Load the model from the HDF5 file
+model = load_model('chatbot_model.h5')
+
+# Functions from your project
+def scrape_goodreads(category):
+    # Your existing scraping code here
+    def scrape_goodreads(category):
+        url = f"https://www.goodreads.com/search?utf8=%E2%9C%93&q={category}&search_type=books"
+        req = requests.get(url, headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36'})
+
+    content = BeautifulSoup(req.content, 'html.parser')
+    books = content.find_all('a', class_='bookTitle')
+    authors = content.find_all('a', class_="authorName")
+
+    data = []
+    for book, author in zip(books, authors):
+        book_name = book.find('span', itemprop='name').text.strip()
+        author_name = author.find('span', itemprop='name').text.strip()
+        data.append({'Book': book_name, 'Author': author_name})
+
+    return json.dumps(data, indent=4)
+
+def clean_up_sentence(sentence):
+    sentence_words = nltk.word_tokenize(sentence)
+    sentence_words = [lemmatizer.lemmatize(word.lower()) for word in sentence_words]
+
+    return sentence_words
+
+def bow(sentence, words, show_details=True):
+    # tokenize the pattern
+    sentence_words = clean_up_sentence(sentence)
+
+    # bag of words - matrix of N words, vocabulary matrix
+    bag = [0] * len(words)
+    for s in sentence_words:
+        for i, w in enumerate(words):
+            if w == s:
+
+                # assign 1 if current word is in the vocabulary position
+                bag[i] = 1
+                if show_details:
+                    print ("found in bag: %s" % w)
+
+    return(np.array(bag))
+
+words=[]
+classes = []
+documents = []
+ignore_words = ['?', '!']
+
+words = [lemmatizer.lemmatize(w.lower()) for w in words if w not in ignore_words]
+words = sorted(list(set(words)))
+
+def predict_class(sentence, model):
+    # filter out predictions below a threshold
+    p = bow(sentence, words,show_details=False)
+    res = model.predict(np.array([p]))[0]
+    ERROR_THRESHOLD = 0.25
+    results = [[i, r] for i, r in enumerate(res) if r > ERROR_THRESHOLD]
+
+    # sort by strength of probability
+    results.sort(key=lambda x: x[1], reverse=True)
+    return_list = []
+    for r in results:
+        return_list.append({"intent": classes[r[0]], "probability": str(r[1])})
+
+    return return_list
+
+
+
+def getResponse(ints, intents_json):
+    tag = ints[0]['intent']
+    list_of_intents = intents_json['intents']
+    for i in list_of_intents:
+        if(i['tag']== tag):
+            if tag == 'book_search':
+              category = input("Sure, I'd be happy to recommend a book. What type of book are you in the mood for? ")
+              result = scrape_goodreads(category)
+            else:
+              result = random.choice(i['responses'])
+            break
+
+    return result
+
+intents = pickle.load(open('intents.pkl','rb'))
+
+def chatbot_response(msg):
+    # Your existing chatbot code here
+
+    ints = predict_class(msg, model)
+    res = getResponse(ints, intents)
+    return res
+
+
+# Streamlit UI
+st.title('Book Recommendation Chatbot')
+
+# Sidebar for user input
+option = st.sidebar.selectbox(
+    'Select an action',
+    ('Book Recommendation', 'Chat with the Bot')
+)
+
+if option == 'Book Recommendation':
+    category = st.text_input('Enter the type of book you want to read:')
+    if st.button('Get Recommendations'):
+        if category:
+            result = scrape_goodreads(category)
+            st.json(result)
+        else:
+            st.warning('Please enter a category.')
+
+elif option == 'Chat with the Bot':
+    msg = st.text_input('You:', '')
+    if st.button('Send'):
+        if msg:
+            response = chatbot_response(msg)
+            st.text_area('Bot:', value=response, height=200, max_chars=None, key=None)
+        else:
+            st.warning('Please enter a message.')
+
+# Streamlit app
