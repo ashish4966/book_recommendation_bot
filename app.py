5980b2c3227f73ac7cb52a683373d7ade12b917f
ashish4966 <111302500+ashish4966@users.noreply.github.com>
2024-05-11 15:31:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -125,13 +125,12 @@ def chatbot_response(msg):
 st.title('Book Recommendation Chatbot')
 
 # Main section for user interaction
-msg = st.text_input('You:', '')
-if st.button('Send'):
-    if msg:
-        response = chatbot_response(msg)
-        st.text_area('Bot:', value=response, height=200, max_chars=None, key=None)
-    else:
-        st.warning('Please enter a message.')
+option = st.selectbox(
+    'Select an action',
+    ('Book Recommendation', 'Chat with the Bot')
+)
+
+if option == 'Book Recommendation':
     category = st.text_input('Enter the type of book you want to read:')
     if st.button('Get Recommendations'):
         if category:
@@ -139,3 +138,15 @@ if st.button('Send'):
             st.markdown(result, unsafe_allow_html=True)
         else:
             st.warning('Please enter a category.')
+
+elif option == 'Chat with the Bot':
+    msg = st.text_input('You:', '')
+    if st.button('Send'):
+        if msg:
+            response = chatbot_response(msg)
+            st.text_area('Bot:', value=response, height=200, max_chars=None, key=None)
+        else:
+            st.warning('Please enter a message.')
+            if msg.lower() == "recommend me a book":
+                response = scrape_goodreads('')
+                st.text_area('Bot:', value=response, height=200, max_chars=None, key=None)
